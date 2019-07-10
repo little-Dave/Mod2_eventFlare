942285510aecad43a248c45597c0a08857db59e9
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-07-10 13:00:17 -0600

    Added route for '/'

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "login", to: "sessions#destroy"
+  get "/", to: redirect("/login")
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
