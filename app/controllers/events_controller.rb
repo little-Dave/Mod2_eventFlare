c4cbad7b105353939032eab319d483e6f864441a
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-07-10 18:35:40 -0600

    Add ability to join an event
    Add join button on each event's show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,12 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
-    
+    @unattended_events = @events.count - @user.user_events.count
   end
 
   def show
     @event = Event.find(params[:id])
+    @user_event = UserEvent.new
     @creator = @event.users.first
   end
 
--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,7 +1,18 @@
 class UserEventsController < ApplicationController
 
-def index
-  @user_events = UserEvent.all
-end
+  def index
+    @user_events = UserEvent.all
+  end
+
+  def create
+    UserEvent.create(allowed_params)
+    redirect_to events_path
+  end
+
+  private
+
+  def allowed_params
+    params.require(:user_event).permit(:user_id, :event_id)
+  end
 
 end
